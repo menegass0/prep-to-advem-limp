6b954692cac169dfc5a0aaa7851a23584f175591
jaozin_farofa <victormenegasso@unisantos.br>
2023-06-29 15:00:18 -0300

    fim do menu

--- 5-menu/src/App.js
+++ 5-menu/src/App.js
@@ -3,11 +3,18 @@ import Menu from './Menu';
 import Categories from './Categories';
 import items from './data';
 
+const allCategories = ['all',...new Set(items.map((item) => item.category))]
+console.log(allCategories);
+
 function App() {
   const [menuItems, setMenuItems] = useState(items);
-  const [catogories, setCategories] = useState([]);
+  const [categories, setCategories] = useState(allCategories);
 
   const filterItems = (category) =>{
+    if(category === 'all'){
+      setMenuItems(items)
+      return;
+    }
     const newItems = items.filter((item) => item.category === category)
     setMenuItems(newItems);
   }
@@ -19,7 +26,7 @@ function App() {
           <h2>our menu</h2>
           <div className="underline"></div>
         </div>
-        <Categories />
+        <Categories filterItems={filterItems} categories={categories}/>
         <Menu items={menuItems} />
       </section>
     </main>
